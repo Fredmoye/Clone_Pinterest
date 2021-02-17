51ec45e68578b1902259af7d18cf47e60e392616
Frederic Konan <frederickonan268@gmail.com>
2021-02-17 11:46:58 +0000

    Ajout Php ini, fouction timestampable et listing par date de creation

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Pin;
 use App\Repository\PinRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,7 +15,16 @@ class PinsController extends AbstractController
      */
     public function index(PinRepository $pinRepository): Response
     {
-        $pins = $pinRepository->findAll();
+        //selectionne les Pin dans l'ordre decroisant
+        $pins = $pinRepository->findBy([], ['createAt' => 'DESC']);
         return $this->render('pins/index.html.twig',compact('pins') );
+    }
+
+    /**
+     * @Route("/pins{id<[0-9]+>}", name="app_pins_show")
+     */
+    public function show (Pin $pin): Response
+    {
+        return $this->render('pins/show.html.twig', compact('pin'));
     }
 }
